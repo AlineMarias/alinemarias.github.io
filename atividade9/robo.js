e17214eafa0ec4364620e78d1080c5a65526ad3e
AlineMarias <139500744+AlineMarias@users.noreply.github.com>
2023-10-19 18:04:56 -0300

    Update robo.js

--- atividade9/robo.js
+++ atividade9/robo.js
@@ -1,1 +1,51 @@
-const robo = document.getElementById("robotron")
+const img = [
+    "robotron",
+    "robotron-amarelo",
+    "robotron-azul", 
+    "robotron-branco", 
+    "robotron-preto", 
+    "robotron-rosa", 
+   "robotron-vermelho"
+];
+var indice = 0;
+
+function esconder(indiceInicial){
+    const robotron = document.getElementById(img[indiceInicial]);
+
+    robotron.style.display="none";
+
+}
+function mostrar(indiceInicial){
+    const robotron = document.getElementById(img[indiceInicial]);
+    
+    robotron.style.display = "block";
+}
+
+function anterior(){
+
+    esconder(indice);
+
+    indice = indice - 1;
+
+    if(indice < 0){
+     indice = img.length - 1;
+}
+
+    mostrar(indice);
+
+}
+
+function proximo(){
+
+    esconder(indice);
+    
+    indice = indice + 1;
+
+   if(indice >= img.length){
+     indice = 0;
+
+   }
+
+   mostrar(indice);
+}
+
