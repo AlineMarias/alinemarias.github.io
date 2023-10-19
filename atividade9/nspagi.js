f082a22d4963ecc90d35df14e08b5563fbb0c448
AlineMarias <139500744+AlineMarias@users.noreply.github.com>
2023-10-19 14:22:29 -0300

    Update nspagi.js

--- atividade9/nspagi.js
+++ atividade9/nspagi.js
@@ -1,15 +1,13 @@
-
 function dividirNome() {
-           
-var nomeCompleto = document.getElementById("nomeCompleto").value;
-
-            
-var partesDoNome = nomeCompleto.split(" ");
-var nome = partesDoNome[0];
-var sobrenome = partesDoNome.slice(1).join(" ");
-
-         
-document.getElementById("nome").value = nome;
-document.getElementById("sobrenome").value = sobrenome;
-}
- 
+ var nomeCompleto = document.getElementById("nomeCompleto").value;
+    
+                
+    var partesDoNome = nomeCompleto.split(" ");
+    var nome = partesDoNome[0];
+    var sobrenome = partesDoNome.slice(1).join(" ");
+    
+             
+    document.getElementById("nome").value = nome;
+    document.getElementById("sobrenome").value = sobrenome;
+    }
+     
